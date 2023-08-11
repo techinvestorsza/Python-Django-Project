2ecb9c2109ca500f02d76cf87d8bfcd1ffed7449
Tariq <techinvestorsza@gmail.com>
2023-08-11 15:02:39 +0200

    Final changes

---
+++ learning_logs/urls.py
@@ -0,0 +1,31 @@
+"""Defines URL patterns for learning_logs."""
+
+from django.urls import path
+from . import views
+
+app_name = 'learning_logs'
+
+urlpatterns = [
+    #Home page
+    path('', views.index, name='index'),
+
+    # Show all topics
+    path('topics', views.topics, name='topics'),
+
+    # Detail page for a single topic
+    path('topics/(?P<topic_id>\d+)', views.topic, name='topic'),
+
+    # Page fr adding a new topic
+    path('new_topic', views.new_topic, name='new_topic'),
+
+    # Page gr adding a new entry
+    path('new_entry/(?p<topic_id>\d+)', views.new_entry, name='new_entry'),
+
+    # Page for editing an entry
+    path('edit_entry/(?P<entry_id>\d+)', views.edit_entry, name='edit_entry'),
+]
+
+# urlpatterns = [
+#     # Home page
+#     url(r'^$', views.index, name='index')
+# ]
