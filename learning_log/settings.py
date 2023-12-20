200bbd58409296bca9579becd808cb924692dd71
techinvestorsza <techinvestorsza@gmail.com>
2023-12-20 19:54:15 +0200

    first commit

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-k+lz3!kb$7f0a%0fj^cm7bhy2u%orn8hhm6k1tu^#9xhbqtu8)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
